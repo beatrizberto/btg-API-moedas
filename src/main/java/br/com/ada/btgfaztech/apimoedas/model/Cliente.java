817bab3fd3b0e46d64d979bce171fef73defc917
lucianasfoliveira <luluzinhasfo@gmail.com>
2023-10-20 18:54:28 -0300

    Atualização Model

--- src/main/java/br/com/ada/btgfaztech/apimoedas/model/Cliente.java
+++ src/main/java/br/com/ada/btgfaztech/apimoedas/model/Cliente.java
@@ -1,2 +1,24 @@
-package br.com.ada.btgfaztech.apimoedas.model;public class Cliente {
+package br.com.ada.btgfaztech.apimoedas.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private String estadoCivil;
+    private String sexo;
+
+    //cliente pode ter várias ordens de compra
+    @OneToMany(mappedBy = "cliente")
+    private List<OrdemCompra> ordensCompra;
+
 }
