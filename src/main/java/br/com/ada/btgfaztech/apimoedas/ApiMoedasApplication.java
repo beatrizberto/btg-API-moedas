05741e3d3b108c99925d66a08a0834701a710c71
Beatriz <beatrizberto@gmail.com>
2023-10-20 10:12:54 -0300

    initial commit

---
+++ src/main/java/br/com/ada/btgfaztech/apimoedas/ApiMoedasApplication.java
@@ -0,0 +1,13 @@
+package br.com.ada.btgfaztech.apimoedas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMoedasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMoedasApplication.class, args);
+	}
+
+}
