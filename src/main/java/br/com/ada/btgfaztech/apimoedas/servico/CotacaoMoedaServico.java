75c499068632d7df6935c73f2db659ce6ccf5efb
Fernanda Soares <fernanda.soares@elogroup.com.br>
2023-10-25 20:01:17 -0300

    get cotacao e save da ordem de compra

--- src/main/java/br/com/ada/btgfaztech/apimoedas/servico/CotacaoMoedaServico.java
+++ src/main/java/br/com/ada/btgfaztech/apimoedas/servico/CotacaoMoedaServico.java
@@ -1,6 +1,9 @@
 package br.com.ada.btgfaztech.apimoedas.servico;
 
 import br.com.ada.btgfaztech.apimoedas.modelo.CotacaoMoeda;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.coyote.Response;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class CotacaoMoedaServico {
@@ -18,65 +22,21 @@ public class CotacaoMoedaServico {
     private final RestTemplate restTemplate = new RestTemplate();
     private final String baseUrl = "https://economia.awesomeapi.com.br/json";
 
-
-//    public ApiCotacaoResponse obterCotacaoMoeda(String moeda) throws IOException {
-//        String url = baseUrl + "/last/" + moeda;
-//        /*ResponseEntity<Map<String, Map<String, Object>>> response = restTemplate.exchange(
-//                url, HttpMethod.GET, null,
-//                new ParameterizedTypeReference<Map<String, Map<String, Object>>>() {}
-//        );*/
-//
-//        ResponseEntity<ApiCotacaoResponse> response = restTemplate.exchange(
-//                url, HttpMethod.GET, null, new ParameterizedTypeReference<ApiCotacaoResponse>() {});
-//
-//        if (response.getStatusCode() == HttpStatus.OK) {
-//            ObjectMapper objectMapper = new ObjectMapper();
-//            ApiCotacaoResponse objetoJava = objectMapper.readValue((DataInput) response.getBody(), ApiCotacaoResponse.class);
-//            return objetoJava;
-//    } else {
-//        //throw new MoedaNaoEncontradaException("Moeda não encontrada.");
-//        throw new RuntimeException();
-//    }
-//
-////        ApiCotacaoResponse cotacoes = response.getBody();
-////        if (cotacoes != null && cotacoes.getCodein().equals(moeda)) {
-////            //Map<String, Object> info = cotacoes.get(moeda);
-////            //return new BigDecimal(info.get("bid").toString());
-////            return cotacoes;
-//
-//
-//    }
-//}
-
-    public Map<String, CotacaoMoeda> obterCotacaoMoeda(String moeda) throws IOException {
+    public CotacaoMoeda obterCotacaoMoeda(String moeda) {
         String url = baseUrl + "/last/" + moeda;
+        ResponseEntity<Map<String, CotacaoMoeda>> response = restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<Map<String, CotacaoMoeda>>() {
+        });
 
-        ResponseEntity<Map<String, CotacaoMoeda>> response =
-                restTemplate.exchange(url, HttpMethod.GET, null, new
-                        ParameterizedTypeReference<Map<String, CotacaoMoeda>>() {});
-        if (response.getStatusCode().is2xxSuccessful() &&
-                response.hasBody()) {
-            return response.getBody();
+        if (response.getStatusCode().is2xxSuccessful() && response.hasBody()) {
+            Map<String, CotacaoMoeda> cotacaoMoeda = response.getBody();
 
-        } else {
-            throw new RuntimeException("Erro ao obter a cotação da moeda.");
+            for (String key : cotacaoMoeda.keySet()) {
+                return cotacaoMoeda.get(key);
+            }
         }
+
+        return null;
     }
 }
-//public List<CotacaoMoeda> obterCotacaoMoeda(String moeda) throws IOException {
-//    String url = baseUrl + "/last/" + moeda;
-//
-//    ResponseEntity<String> response = restTemplate.exchange(
-//            url, HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}
-//    );
-//
-//    if (response.getStatusCode().is2xxSuccessful()) {
-//        List<CotacaoMoeda> cotacoes = response.getBody();
-//        return cotacoes;
-//    } else {
-//        throw new RuntimeException("Erro ao obter a cotação da moeda.");
-//    }
-//}
-
 
 
--- src/main/java/br/com/ada/btgfaztech/apimoedas/servico/OrdemCompraServico.java
+++ src/main/java/br/com/ada/btgfaztech/apimoedas/servico/OrdemCompraServico.java
@@ -1,10 +1,21 @@
 package br.com.ada.btgfaztech.apimoedas.servico;
 
+import br.com.ada.btgfaztech.apimoedas.controlador.dto.OrdemCompraRequest;
+import br.com.ada.btgfaztech.apimoedas.controlador.dto.OrdemCompraResponse;
+import br.com.ada.btgfaztech.apimoedas.modelo.Cliente;
+import br.com.ada.btgfaztech.apimoedas.modelo.CotacaoMoeda;
+import br.com.ada.btgfaztech.apimoedas.modelo.OrdemCompra;
 import br.com.ada.btgfaztech.apimoedas.repositorio.IClienteRepositorio;
 import br.com.ada.btgfaztech.apimoedas.repositorio.IOrdemCompraRepositorio;
+import br.com.ada.btgfaztech.apimoedas.utils.ClienteConversor;
+import br.com.ada.btgfaztech.apimoedas.utils.OrdemCompraConversor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class OrdemCompraServico {
@@ -15,4 +26,32 @@ public class OrdemCompraServico {
     @Autowired
     private IOrdemCompraRepositorio ordemCompraRepositorio;
 
+    @Autowired
+    private CotacaoMoedaServico cotacaoMoedaServico;
+
+
+    public OrdemCompraResponse criarOrdemCompra(OrdemCompraRequest ordemCompraRequest) {
+        //buscando cotacao
+        String moeda = ordemCompraRequest.getTipoMoeda();
+        CotacaoMoeda cotacao = cotacaoMoedaServico.obterCotacaoMoeda(moeda);
+        if(cotacao == null) {
+            throw new RuntimeException();
+        }
+
+        BigDecimal valorCotacao = cotacao.getAsk();
+
+        //calculo valor total
+        BigDecimal valorTotal = valorCotacao.multiply(ordemCompraRequest.getValorMoedaEstrangeira());
+
+        //buscando cliente
+        Optional<Cliente> cliente = clienteRepositorio.findByCpf(ordemCompraRequest.getCpf());
+        if(!cliente.isPresent()) {
+            throw new RuntimeException();
+        }
+
+        OrdemCompra ordemCompra = OrdemCompraConversor.toEntity(ordemCompraRequest, valorCotacao, valorTotal, cliente.get());
+
+        return OrdemCompraConversor.toResponse(ordemCompraRepositorio.save(ordemCompra));
+        
+    }
 }
